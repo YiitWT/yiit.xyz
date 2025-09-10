28bb9cc75a97854434ecdb88a3cfd6a667cfa516
YiitWT <yigit.best@outlook.com>
2025-09-10 14:38:43 +0300

    TranslationProviders.tsx Edited

--- src/i18n/TranslationProvider.tsx
+++ src/i18n/TranslationProvider.tsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useState } from 'react';
 import type { ReactNode } from 'react';
 import en from './en.json';
-import tr from './tr.json';
 
 type Translations = typeof en;
 
